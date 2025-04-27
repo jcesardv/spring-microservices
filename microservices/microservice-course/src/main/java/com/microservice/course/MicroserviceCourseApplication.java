20dd219a351975c1ea7d280870c7e0210a8beea9
jcesardv <julio.quispe2022@outlook.com>
2025-04-27 18:42:20 +0000

    feat(base): Add gateway config

--- microservices/microservice-course/src/main/java/com/microservice/course/MicroserviceCourseApplication.java
+++ microservices/microservice-course/src/main/java/com/microservice/course/MicroserviceCourseApplication.java
@@ -1,15 +1,17 @@
-package com.microservice.gateway.microservice_gateway;
+package com.microservice.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
-public class MicroserviceGatewayApplication {
+public class MicroserviceCourseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceGatewayApplication.class, args);
+		SpringApplication.run(MicroserviceCourseApplication.class, args);
 	}
 
 }
--- microservices/microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
+++ microservices/microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-student", url = "localhost:8090")
+@FeignClient(name = "msvc-student", url = "localhost:8080")
 public interface StudentClient {
 
     @GetMapping("/api/student/search-by-course/{courseId}")
